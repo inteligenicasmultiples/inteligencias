3d73b61fa7bd71ae3d385688b6d1b23fe0a7cced
Inteligencias <soporte.inteligenicasmultiples@gmail.com>
2016-01-24 15:08:19 -0500

    Mejoras

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,12 @@
 @extends('layouts.app')
 
+
 @section('content')
 <div class="container spark-screen">
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Bienvenido!!</div>
+                <div class="panel-heading">Bienvenido!!!</div>
                 <div class="panel-body">
 
                 </div>
